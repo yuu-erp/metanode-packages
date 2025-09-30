ca43b24de77c3f5857b0271d6993f4a1296a2901
hoanmini <>
2025-09-30 11:57:40 +0700

    feat: exceptions package publish ✅

---
+++ packages/exceptions/src/index.ts
@@ -0,0 +1,9 @@
+// Export base
+export { ExceptionBase } from "./exceptions/exception.base";
+export type { NormalizedException } from "./exceptions/exception.base";
+
+// Export all predefined exceptions
+export * from "./exceptions/exceptions";
+
+// Export codes nếu cần dùng bên ngoài
+export * from "./exceptions/exception.codes";
---
+++ packages/exceptions/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  outDir: "dist",
+  // ép ESM build ra .mjs thay vì .js
+  outExtension({ format }) {
+    return format === "esm" ? { js: ".mjs" } : { js: ".cjs" };
+  },
+});
